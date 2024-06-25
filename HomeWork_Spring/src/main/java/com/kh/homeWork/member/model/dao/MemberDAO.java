39f563835e4c6bcce894463e4436181b8a4593fc
재영 <jyjh7555@naver.com>
2024-06-25 09:38:35 +0900

    로그인추가 controller, service, dao, mapper navbar(90줄empName -> nickName)수정

--- HomeWork_Spring/src/main/java/com/kh/homeWork/member/model/dao/MemberDAO.java
+++ HomeWork_Spring/src/main/java/com/kh/homeWork/member/model/dao/MemberDAO.java
@@ -1,8 +1,15 @@
 package com.kh.homeWork.member.model.dao;
 
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
+
+import com.kh.homeWork.member.model.vo.Member;
 
 @Repository("mDAO")
 public class MemberDAO {
+
+	public Member loginCheck(SqlSessionTemplate sqlSession, Member m) {
+		return sqlSession.selectOne("memberMapper.loginCheck", m);
+	}
 
 }
