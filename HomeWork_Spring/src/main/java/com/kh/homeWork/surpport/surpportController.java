c8325eaf05f62200d5bef6b1605297cff0463443
user1 <user1@DESKTOP-DV6J0NL>
2024-07-04 18:05:50 +0900

    후원 결제 건드리다가 멘붕옴 다음에할건 폼태그 건드리기

--- HomeWork_Spring/src/main/java/com/kh/homeWork/surpport/surpportController.java
+++ HomeWork_Spring/src/main/java/com/kh/homeWork/surpport/surpportController.java
@@ -1,13 +1,26 @@
 package com.kh.homeWork.surpport;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.kh.homeWork.member.model.vo.Member;
 
 @Controller
 public class surpportController {
 	
 	@RequestMapping("surpport.su")
-	public String sponserMain() {
+	public String sponserMain(HttpSession session,Model model) {
+		Member m = (Member)session.getAttribute("loginUser");
+		if(m != null) {
+			int mid = Integer.parseInt(m.getPhone().substring(4, 8));
+			int last = Integer.parseInt(m.getPhone().substring(9, 13));
+			model.addAttribute("midPhone",mid);
+			model.addAttribute("lastPhone",last);
+		}
+		
 		return "surpportMain";
 	}
 	
