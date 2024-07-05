85dbd4cde48cf4448e41bb426d59f2ddda4ba7fd
user1 <user1@DESKTOP-DV6J0NL>
2024-07-05 14:54:19 +0900

    중간점검

--- HomeWork_Spring/src/main/java/com/kh/homeWork/surpport/surpportController.java
+++ HomeWork_Spring/src/main/java/com/kh/homeWork/surpport/surpportController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.kh.homeWork.member.model.vo.Member;
 
@@ -22,6 +23,13 @@ public class surpportController {
 		}
 		
 		return "surpportMain";
+	}
+	
+	@RequestMapping("successPay.su")
+	public String successPay(@RequestParam(value="success") boolean success,@RequestParam("merchantUid") String uId) {
+		System.out.println(success);
+		System.out.println(uId);
+		return "terms";
 	}
 	
 }
